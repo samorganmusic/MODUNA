a9e4f2fea202deb71935cf2886096d8795457431
Sam <Morgan>
2020-02-24 22:41:15 +1300

    YUP
    echo "# MODUNA" >> README.md
    git init
    git add README.md
    git commit -m "first commit"
    git remote add origin https://github.com/samorganmusic/MODUNA.git
    git push -u origin master

---
+++ MODUNA/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MODUNA
+//
+//  Created by Sam Morgan on 24/02/20.
+//  Copyright © 2020 Sam Morgan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
